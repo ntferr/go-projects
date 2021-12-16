206f89a69a63db8e95a03b2b35c0ff04ba3af3df
Nathan Ferreira <ntfm95@gmail.com>
2021-12-16 18:47:02 -0300

    Add files via upload

---
+++ nested-templates/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "index.gohtml", nil)
+	if err != nil {
+		log.Fatalln("Template didn't execute: ", err)
+	}
+}
